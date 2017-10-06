6469cf9504237d11c67e1f5442abfd4760ef6162
kecorbin <kecorbin@cisco.com>
2017-10-06 14:28:59 -0500

    change server backend to tornado

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -2,10 +2,10 @@
 
 import connexion
 from .encoder import JSONEncoder
-from flask.ext.cors import CORS
+from flask_cors import CORS
 
 if __name__ == '__main__':
-    app = connexion.App(__name__, specification_dir='./swagger/')
+    app = connexion.App(__name__, specification_dir='./swagger/', server='tornado')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Server Provisoning API '})
     cors = CORS(app.app, resources={r"/api/*": {"origins": "*"}})
